934914d41d1391967f08e153167e58a9947ca7a5
Thomas Marosz <thomas.c.marosz@vanderbilt.edu>
2019-02-03 15:55:04 -0600

    initial broker implementation

--- api/broker.py
+++ api/broker.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
 
+import threading
+import uuid
+import zmq
+from common import config
+
+class Broker:
+
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.setupBroker()
+        
+    def __setupBroker(self):
+        frontendPort = config()["broker"]["frontendForwarder"]
+        backendPort = config()["broker"]["backendForwarder"]
+
+        try:
+            self.context = zmq.Context()
+
+            frontend = self.context.socket(zmq.XSUB)
+            frontend.bind(f"tcp://*:{frontendPort}")
+
+            backend = self.context.socket(zmq.XPUB)
+            backend.bind(f"tcp://*:{backendPort}")
+
+            print("Bound frontend and backend of Broker")
+
+            zmq.proxy(frontend, backend)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            pass
+            print("Shutting down broker")
+            frontend.close()
+            backend.close()
+            self.context.term()
+
+
+    def setupBroker(self):
+        self.broker = threading.Thread(target=self.__setupBroker)
+        self.broker.start()
+
+
+if __name__ == "__main__":
+
+    b = Broker()
+    print("got here")
 
